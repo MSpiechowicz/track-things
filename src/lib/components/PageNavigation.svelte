b540ffacf21b24a5a1a5a7dfbd0f88897561823c
Maciej Spiechowicz <spm1fr@bosch.com>
2025-02-09 20:09:09 +0100

    Update PageNavigation with dashboard link and refined layout
    - Add Dashboard link button in navigation
    - Adjust spacing and layout of user navigation elements
    - Remove redundant display name text in mobile view

--- src/lib/components/PageNavigation.svelte
+++ src/lib/components/PageNavigation.svelte
@@ -9,28 +9,30 @@
 <nav class="flex h-[96px] items-center justify-between border-b border-white/15 md:px-8">
 	<a
 		href={id ? '/dashboard' : '/'}
-    data-sveltekit-reload
+		data-sveltekit-reload
 		class="flex flex-shrink-0 cursor-pointer items-center hover:no-underline"
 	>
 		<div class="flex text-3xl font-bold sm:hidden">
 			<span class="text-white">t</span>
-      <div class="flex justify-center items-center">
-        <IconFoots />
-      </div>
+			<div class="flex items-center justify-center">
+				<IconFoots />
+			</div>
 			<span class="text-blue-600">t</span>
 		</div>
 		<div class="hidden text-3xl font-bold sm:flex">
 			<span class="text-white">track</span>
-      <div class="flex justify-center items-center">
-        <IconFoots additionalClass="h-auto w-auto" />
-      </div>
+			<div class="flex items-center justify-center">
+				<IconFoots additionalClass="h-auto w-auto" />
+			</div>
 			<span class="text-blue-600">things</span>
 		</div>
 		<span class="ml-2 rounded-full bg-gray-200 px-2 py-1 text-xs text-gray-700">ALPHA</span>
 	</a>
 	{#if id}
-		<div class="flex items-center gap-4 text-white">
-			<span class="hidden text-lg sm:block">{displayName}</span>
+		<div class="flex items-center gap-2 text-white">
+			<Button variant="link" href="/dashboard" data-sveltekit-reload class="text-white"
+				>Dashboard</Button
+			>
 			<div class="hidden sm:block">
 				<PageUserMenu {id} {displayName} {email} {name} />
 			</div>
